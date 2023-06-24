ca7139faa4515d26d46f59ee4b78a10b15a1e0a0
snroeung <sarenna.roeung@gmail.com>
2023-06-23 20:53:34 -0400

    Everything is on a single "page"

--- codebase/src/App.js
+++ codebase/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import ContactInformation from './pages/ContactInformation';
 import ResumeAttachment from './pages/ResumeAttachment';
 import Experiences from './pages/Experiences';
@@ -10,32 +9,19 @@ function App() {
   const [savedExperiences, setSavedExperiences] = useState({});
 
   function handleSaveAll() {
-    //this will change once backend is configured
-    console.log("Contact Information:", savedContactInformation);
-    console.log("Experiences: ", savedExperiences);
+    console.log(savedContactInformation);
   }
 
   return (
-    <Tabs>
-      <TabPanel>
-        {<ContactInformation setSavedContactInformation={setSavedContactInformation}/>}
-      </TabPanel>
-      <TabPanel>
-        {<ResumeAttachment />}
-      </TabPanel>
-      <TabPanel>
-        {<Experiences setSavedExperiences={setSavedExperiences}/>}
-      </TabPanel>
-      <TabList className="flex justify-center m-4">
-        <Tab className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">1</Tab>
-        <Tab className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">2</Tab>
-        <Tab className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">3</Tab>
-      </TabList>
-      <div className="flex justify-center my-2">
+    <div>
+      {<ContactInformation setSavedContactInformation={setSavedContactInformation}/>}
+      {<Experiences setSavedExperiences={setSavedExperiences}/>}
+      {<ResumeAttachment />}
+      <div className="flex justify-center">
           <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
           onClick={() => handleSaveAll()}>Save</button>
       </div>
-    </Tabs>
+    </div>
   );
 }
 
--- codebase/src/pages/ResumeAttachment.js
+++ codebase/src/pages/ResumeAttachment.js
@@ -12,18 +12,18 @@ function ResumeAttachment() {
                 </h2>
             </header>
             <form>
-                <div className="flex flex-row justify-center my-2">
+                <div className="flex flex-row justify-center my-4">
                     <label>
-                    <span className="sr-only">Upload resume</span>
-                    <input type="file" className="block w-full text-sm text-slate-500
+                        <span className="sr-only">Upload resume</span>
+                        <input type="file" className="block w-full text-sm text-slate-500
       file:mr-4 file:py-2 file:px-4
       file:rounded-full file:border-0
       file:text-sm file:font-semibold
       file:bg-violet-50 file:text-violet-700
       hover:file:bg-violet-100
     "
-    onChange={(e) => setResume(e.target.files[0])}    />
-                </label>
+                            onChange={(e) => setResume(e.target.files[0])} />
+                    </label>
                 </div>
             </form>
         </div>
